c309900a5422b5b27bcc65d0dfe1e45baacd0857
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-03-05 01:20:52 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,12 @@
 def unsafe?(speed)
 	if speed > 60 || speed < 40
-		return true
+		return true;
 	else
-		return false
-end
+		return false;
+	end
 end
 
+
 def not_safe?(speed)
-	if speed > 60 || speed < 40
-		return true
-	else
-		return false
-	end
+	speed > 60 || speed < 40 ? true : false;
 end
